83d55b6fc402258aa056f886d62cc9fa53a44828
Mirko <mirkocaccavone03@gmail.com>
2025-02-17 19:11:52 +0100

    [feat] add Main component to render AccordionList

---
+++ src/components/MainComponents.jsx
@@ -0,0 +1,9 @@
+import AccordionList from "./AccordionList";
+
+export default function Main() {
+    return (
+        <main className="container">
+            <AccordionList />
+        </main>
+    );
+}
